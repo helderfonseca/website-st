03823d3f7b31efe61c696ec13523c4b53fa1c827
Helder Fonseca <helder2391@gmail.com>
2022-11-21 16:36:29 -0100

    add tag manager and GA to LP

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100&display=swap" rel="stylesheet"></link>
+        <Script
+            id='gtmws-st'
+            strategy='beforeInteractive'
+          >
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M73PCLM');`}
+        </Script>
       </Head>
       <body>
         <Main />
@@ -30,6 +40,8 @@ export default function Document() {
         >
 
         </Script>*/}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M73PCLM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
       </body>
     </Html>
   )
